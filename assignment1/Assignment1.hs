845c24d791bd6e36db46316d541e61ca3c2693b0
Tony Malzhacker <tony@malzhacker.de>
2014-11-30 00:49:46 +0100

    Init

---
+++ assignment1/Assignment1.hs
@@ -0,0 +1,82 @@
+
+--
+-- Enter name and matriculation number here!
+--
+
+module Assignment1 where
+
+import Data.Char
+
+
+--
+-- Task 1.1
+--
+
+
+findPos :: String -> [String] -> Int
+findPos _ _ = -1   -- DUMMY: replace by implementation
+
+
+
+findCharStrings :: Char -> [String] -> [String]
+findCharStrings _ _ = []  -- DUMMY: replace by implementation
+
+
+
+--
+-- Task 1.2
+--
+
+sortStrings :: [String] -> [String]
+sortStrings _ = []   -- DUMMY: replace by implementation
+
+
+-- Explain here briefly, how your algorithm works!
+-- 
+
+
+
+--
+-- Task 1.3
+--
+
+
+nameScore :: String -> [String] -> Integer
+nameScore _ _ = 0   -- DUMMY: replace by implementation
+
+
+
+
+--
+-- Task 2.1
+--
+
+
+readConsole :: IO ()
+readConsole = return ()   -- DUMMY: replace by implementation
+
+
+
+
+--
+-- Task 2.2
+--
+
+
+readFileTokens :: FilePath -> IO [String]
+readFileTokens _ = return []   -- DUMMY: replace by implementation
+
+
+
+writeFileTokens :: [String] -> FilePath -> IO ()
+writeFileTokens _ _ = return ()   -- DUMMY: replace by implementation
+
+
+
+main :: IO ()
+main = return ()   -- DUMMY: replace by implementation
+
+
+
+
+
